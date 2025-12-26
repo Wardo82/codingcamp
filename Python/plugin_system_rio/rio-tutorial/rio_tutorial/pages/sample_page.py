3a7bd096f48dcbc51d0062d436e7ecf271edc77c
Wardo82 <wardo@Wardos-MacBook.local>
2025-12-26 01:35:27 +0100

    feature(rio): Started rio tic-tac-toe tutorial

---
+++ Python/plugin_system_rio/rio-tutorial/rio_tutorial/pages/sample_page.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from dataclasses import KW_ONLY, field
+import typing as t
+
+import rio
+
+from .. import components as comps
+
+@rio.page(
+    name="Tic Tac Toe",
+    url_segment="",
+)
+class TicTacToePage(rio.Component):
+    fields: list[t.Literal["X", "O", ""]] = [""] * 9
+
+    def build(self) -> rio.Component:
+        # Spawn components for the fields
+        field_components: list[rio.Component] = []
+
+        for index, field in enumerate(self.fields):
+            field_components.append(
+                comps.Field(
+                    value=field,
+                )
+            )
+
+        # Arrange all components in a grid
+        return rio.Grid(
+            field_components[0:3],
+            field_components[3:6],
+            field_components[6:9],
+            row_spacing=1,
+            column_spacing=1,
+            align_x=0.5,
+        )
