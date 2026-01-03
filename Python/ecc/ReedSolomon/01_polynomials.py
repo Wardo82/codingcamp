6bbf66b01a8691b45342d6ef49fa582e12d164ff
Wardo82 <wardoandres@gmail.com>
2026-01-03 21:24:27 +0100

    feat(ecc): Second part of RS tutorial with encoding and decoding, added paper and new utils.py function

--- Python/ecc/ReedSolomon/01_polynomials.py
+++ Python/ecc/ReedSolomon/01_polynomials.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import numpy.polynomial as poly
+
+from utils import *
 
 # Algebra de polinomios:
 f = poly.Polynomial([3, 2, 5, -4])  # f(x)
@@ -18,53 +19,12 @@ quo, rem = divmod(f, g)
 print(f"    Cociente: {quo} - Resto: {rem}")
 
 # Minima informacion para definir un polinomio
-def plot_polynom(f, points, begin: int = -2, end: int = 5):
-    # Dominio continuo para el plot
-    x = np.linspace(begin, end, 400)
-    y = f(x)
-
-    # Puntos específicos a marcar
-    y_pts = f(points)
-
-    # Plot
-    plt.figure()
-    plt.plot(x, y, label="f(x)")
-    plt.scatter(points, y_pts, zorder=3, label="Puntos evaluados")
-    plt.axhline(0)
-    plt.axvline(0)
-    plt.grid(True)
-    plt.legend()
-
-    plt.show()
-
-
-# Conseguir el polinomio a partir de los puntos
-def build_vandermonde(f, x):
-    """
-    f: numpy.polynomial.Polynomial
-    x: iterable of evaluation points
-
-    returns:
-        A: Vandermonde matrix
-        b: f evaluated at x
-    """
-    x = np.asarray(x, dtype=float)
-    degree = f.degree()
-
-    A = np.zeros((len(x), degree + 1), dtype=float)
-
-    for i, xi in enumerate(x):
-        for j in range(degree + 1):
-            A[i, j] = xi ** j
-
-    b = f(x)
-    return A, b
-
 f = poly.Polynomial([2, 3, -5, 1])
 
 x = np.array([-1, 0, 1, 2])
 plot_polynom(f, x)
-A, b = build_vandermonde(f, x)
+A = build_vandermonde(x)
+b = f(x)
 print("Queremos resolver el sistema de equaciones Ac = b")
 print("A:")
 print(A)
@@ -75,7 +35,8 @@ print(f"Coeficientes: c = A^-1 b = {coeffs}")
 
 x = np.array([1, 2, 3, 4])
 plot_polynom(f, x)
-A, b = build_vandermonde(f, x)
+A = build_vandermonde(x)
+b = f(x)
 print("A:")
 print(A)
 print(f"b: {b}")
